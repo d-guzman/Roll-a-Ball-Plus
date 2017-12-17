cba72062afdf949744fafb317c5f89fa1f092e73
d-guzman <dghost121@gmail.com>
2017-12-17 01:31:30 -0800

    Reorganization, Score, and Collectibles
    - Reorganized the 'Prefabs' folder to reduce clutter.
    - Implemented a basic score trackers.
    - Implemented a simple collectible.

---
+++ Assets/Scripts/CollectibleRotator.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectibleRotator : MonoBehaviour {
+	void Update () {
+        transform.Rotate(new Vector3(10,35,45) * Time.deltaTime);
+	}
+}
---
+++ Assets/Scripts/PlayerScore.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerScore : MonoBehaviour {
+    [Tooltip("Link the player score to the score text object in the UI.")]
+    public UnityEngine.UI.Text scoreText;
+
+    [SerializeField]
+    private int playerScore;
+
+	// Update is called once per frame
+	void Update () {
+        scoreText.text = "Score: " + playerScore;
+	}
+
+    void OnTriggerEnter(Collider other) {
+        if (other.tag == "Collectible") {
+            playerScore += 100;
+            other.gameObject.SetActive(false);
+        }
+    }
+}
