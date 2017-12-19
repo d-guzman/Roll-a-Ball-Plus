974c5fdfe527897b7471d037dd3576002a6e88ad
d-guzman <dghost121@gmail.com>
2017-12-18 20:16:25 -0800

    Added Menu with minor functionality
    - Created a new scene, 'Test_MainMenu'.
    - Can load 'Test_Level' from 'Test_MainMenu'.
    - Can quit the game from 'Test_MainMenu' when running a build.

---
+++ Assets/Scripts/MenuScripts/StartGame.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartGame : MonoBehaviour {
+    public void playGame() {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Test_Level");
+    }
+}
